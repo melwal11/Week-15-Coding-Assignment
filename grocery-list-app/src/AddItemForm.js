c2e649bd0d38b58a18ff9cc47502e3bae98922de
Melwal11 <Melanie@MacBook-Pro.local>
2023-07-16 17:56:26 -0700

    Final Commit

--- grocery-list-app/src/AddItemForm.js
+++ grocery-list-app/src/AddItemForm.js
@@ -15,7 +15,23 @@ class AddItemForm extends Component {
     const { name, quantity } = this.state;
     
     // Make API request to create a new grocery item
-    // Clear the form fields and update the grocery list
+    fetch('http://localhost:8000/groceryItems', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({ name, quantity })
+    })
+    .then(response => response.json())
+    .then(newItem => {
+      // Clear the form fields and update the grocery list state
+      this.setState({ name: '', quantity: '' });
+      // Call a function or update the state in the parent component to reflect the newly added item
+    })
+    .catch(error => {
+      // Handle any error that occurred during the API request
+      console.error('Error adding item:', error);
+    });
   };
 
   render() {
